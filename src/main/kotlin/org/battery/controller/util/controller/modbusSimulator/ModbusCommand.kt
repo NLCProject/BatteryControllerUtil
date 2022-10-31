ecc71c501e4316606958983f0f8e52b247c4f00c
mgraf <mgraf3@gmx.at>
2022-10-31 11:45:56 +0100

    ID added to modbus command

--- src/main/kotlin/org/battery/controller/util/controller/modbusSimulator/ModbusCommand.kt
+++ src/main/kotlin/org/battery/controller/util/controller/modbusSimulator/ModbusCommand.kt
@@ -11,6 +11,11 @@ enum class ModbusCommand(
     /**
      *
      */
+    var id: String,
+
+    /**
+     *
+     */
     var i18nKey: I18nKey,
 
     /**
@@ -18,10 +23,39 @@ enum class ModbusCommand(
      */
     var accessType: AccessType
 ) {
-    ModbusFirmwareVersion(i18nKey = I18nKey.CommandModbusFirmwareVersion, accessType = AccessType.Read),
-    NumberOfRegisters(i18nKey = I18nKey.CommandNumberOfRegisters, accessType = AccessType.Read),
-    Manufacturer(i18nKey = I18nKey.CommandManufacturer, accessType = AccessType.Read),
-    HardwareModel(i18nKey = I18nKey.CommandHardwareModel, accessType = AccessType.Read),
-    SerialNumber(i18nKey = I18nKey.CommandSerialNumber, accessType = AccessType.Read),
-    HardwareFirmwareVersion(i18nKey = I18nKey.CommandHardwareFirmwareVersion, accessType = AccessType.Read)
+    ModbusFirmwareVersion(
+        id = "dd40e1c7-b10d-405f-8f12-970b9d53227a",
+        i18nKey = I18nKey.CommandModbusFirmwareVersion,
+        accessType = AccessType.Read
+    ),
+
+    NumberOfRegisters(
+        id = "dc2fa557-c373-44ff-93c7-a88f22d733c3",
+        i18nKey = I18nKey.CommandNumberOfRegisters,
+        accessType = AccessType.Read
+    ),
+
+    Manufacturer(
+        id = "70057dec-4435-4b53-9c25-d28e6a5cf688",
+        i18nKey = I18nKey.CommandManufacturer,
+        accessType = AccessType.Read
+    ),
+
+    HardwareModel(
+        id = "1ac41926-7780-4a27-aff1-284325d163b6",
+        i18nKey = I18nKey.CommandHardwareModel,
+        accessType = AccessType.Read
+    ),
+
+    SerialNumber(
+        id = "74133a83-a994-4048-9c8f-a86c37125221",
+        i18nKey = I18nKey.CommandSerialNumber,
+        accessType = AccessType.Read
+    ),
+
+    HardwareFirmwareVersion(
+        id = "0a97e033-6046-4d8c-a724-d8db596a13cc",
+        i18nKey = I18nKey.CommandHardwareFirmwareVersion,
+        accessType = AccessType.Read
+    )
 }
